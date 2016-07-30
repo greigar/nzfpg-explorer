144adc63557776e1ddb65ac9a2ab451a5e1f5ea3
greigar <greigar@gmail.com>
2016-07-31 11:08:33 +1200

    Nicer price heat map

--- helpers.R
+++ helpers.R
@@ -16,9 +16,9 @@ prices <- function(node, trading_period_min, trading_period_max, price_min, pric
                   Price          <= price_max                    &
                   Trading_date   >= as.POSIXct(trading_date_min) &
                   Trading_date   <= as.POSIXct(trading_date_max)) %>%
-    ggplot(aes(Trading_date, Trading_period)) + 
-      geom_point(aes(size = Price, colour = Price)) + 
-      scale_colour_gradient(low = "green", high = "red")
+    ggplot(aes(x=Trading_date, y=Trading_period)) + 
+      geom_tile(aes(fill=Price), colour="grey") + 
+      scale_fill_gradient(low="grey", high="red")
 }
 
 kwhs <- function(node, trading_period_min, trading_period_max, trading_date_min, trading_date_max) {
