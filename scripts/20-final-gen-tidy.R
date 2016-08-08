08f69ee6314881c6b1cc93b32021639eeb38c777
greigar <greigar@gmail.com>
2016-08-08 22:22:25 +1200

    Closure for file readers

--- scripts/20-final-gen-tidy.R
+++ scripts/20-final-gen-tidy.R
@@ -11,45 +11,54 @@ library(lubridate) # for date arithmetic in the config file
 # Read data in from the CSV files
 # __________________________________________________
 
-# Generation file column spec
-gen_cols <- cols(.default     = col_double(),
-                 Site_Code    = col_character(),
-                 POC_Code     = col_character(),
-                 Nwk_Code     = col_character(),
-                 Gen_Code     = col_character(),
-                 Fuel_Code    = col_character(),
-                 Tech_Code    = col_character(),
-                 Trading_date = col_date(format = ""),
-                 TP49         = col_double(),
-                 TP50         = col_double())
+read_files <- function(filetype) {
 
-# Final prices file column spec
-fp_cols <- cols(Trading_date   = col_date(format = ""),
-                Trading_period = col_integer(),
-                Node           = col_character(),
-                Price          = col_double())
+  if (filetype == "generation") {
+    filter <- "*Generation_MD.csv"
 
-# Nice function to load up multiple files into one data frame
-# From http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
-#      http://www.r-bloggers.com/author/tony-cookson/
-multmerge = function(thepath, filter, col_spec){
-  filenames = list.files(path = thepath, filter, full.names = TRUE)
-  datalist  = lapply(filenames, function(x) {read_csv(file = x, col_types = col_spec)})
-  Reduce(function(x,y) {rbind(x, y)}, datalist)
+    # Generation file column spec
+    col_spec <- cols(.default     = col_double(),
+                     Site_Code    = col_character(),
+                     POC_Code     = col_character(),
+                     Nwk_Code     = col_character(),
+                     Gen_Code     = col_character(),
+                     Fuel_Code    = col_character(),
+                     Tech_Code    = col_character(),
+                     Trading_date = col_date(format = ""),
+                     TP49         = col_double(),
+                     TP50         = col_double())
+  } else {
+    filter <- "*Final_prices.csv"
+
+    # Final prices file column spec
+    col_spec <- cols(Trading_date   = col_date(format = ""),
+                     Trading_period = col_integer(),
+                     Node           = col_character(),
+                     Price          = col_double())
+  }
+
+  thepath  <- "data/downloads"
+
+  # Nice function to load up multiple files into one data frame
+  # From http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
+  #      http://www.r-bloggers.com/author/tony-cookson/
+  multmerge = function(){
+    filenames = list.files(path = thepath, filter, full.names = TRUE)
+    datalist  = lapply(filenames, function(x) {read_csv(file = x, col_types = col_spec)})
+    Reduce(function(x,y) {rbind(x, y)}, datalist)
+  }
 }
 
-generation  <- multmerge("data/downloads", "*Generation_MD.csv", gen_cols)
-finalprices <- multmerge("data/downloads", "*Final_prices.csv",  fp_cols)
+read_csv_generation  <- read_files("generation")
+read_csv_finalprices <- read_files("finalprices")
+
+generation  <- read_csv_generation()
+finalprices <- read_csv_finalprices()
 
 # __________________________________________________
 #
 # Tidy it up 
 # __________________________________________________
-
-# Get nodes that are in both final prices and generation
-fpnodes     <- sort(unique(finalprices$Node))
-gnnodes     <- sort(unique(generation$POC_Code))
-union_nodes <- union(fpnodes, gnnodes)
 
 # Transform into tidy data
 generation <- generation %>%
@@ -64,6 +73,11 @@ generation <- generation %>%
 generation_aggr <- generation %>%
   group_by(Trading_date, Trading_period, Node) %>%
   summarise(kwh = sum(kwh))
+
+# Get nodes that are in both final prices and generation
+fpnodes     <- sort(unique(finalprices$Node))
+gnnodes     <- sort(unique(generation$Node))
+union_nodes <- union(fpnodes, gnnodes)
 
 # __________________________________________________
 #
@@ -84,7 +98,7 @@ nzfpg_config <- list("min_price"  = min_price,
                      "max_price"  = max(finalprices$Price),
                      "min_date"   = min(finalprices$Trading_date),
                      "max_date"   = max_date,
-                     "from_date"  = max_date - 14,
+                     "from_date"  = max_date - 7,
                      "to_date"    = max_date,
                      "from_price" = min_price,
                      "to_price"   = quantile(finalprices$Price)[[3]])
