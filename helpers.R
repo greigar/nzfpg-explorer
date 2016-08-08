a77f5d873c51e7252e646ca36caa4ca97555cca9
greigar <greigar@gmail.com>
2016-08-08 22:04:46 +1200

    Use readr to parse CSV files

--- helpers.R
+++ helpers.R
@@ -14,8 +14,8 @@ prices <- function(node, trading_period_min, trading_period_max, price_min, pric
                   Trading_period <= trading_period_max           &
                   Price          >= price_min                    &
                   Price          <= price_max                    &
-                  Trading_date   >= as.POSIXct(trading_date_min) &
-                  Trading_date   <= as.POSIXct(trading_date_max)) %>%
+                  Trading_date   >= as.Date(trading_date_min) &
+                  Trading_date   <= as.Date(trading_date_max)) %>%
     ggplot(aes(x=Trading_date, y=Trading_period)) + 
       geom_tile(aes(fill=Price), colour="grey") + 
       scale_fill_gradient(low="white", high="red")
@@ -27,8 +27,8 @@ kwhs <- function(node, trading_period_min, trading_period_max, trading_date_min,
                   Node           == node                         &
                   Trading_period >= trading_period_min           &
                   Trading_period <= trading_period_max           &
-                  Trading_date   >= as.POSIXct(trading_date_min) &
-                  Trading_date   <= as.POSIXct(trading_date_max)) %>%
+                  Trading_date   >= as.Date(trading_date_min) &
+                  Trading_date   <= as.Date(trading_date_max)) %>%
     ggplot(aes(Trading_date, Trading_period)) + 
       geom_point(aes(size = kwh, colour = kwh)) + 
       scale_colour_gradient(low = "green", high = "red")
--- scripts/20-final-gen-tidy.R
+++ scripts/20-final-gen-tidy.R
@@ -1,26 +1,45 @@
 library(reshape2)  # for melt-ing
 library(dplyr)     # for changing data frames and aggregation
 library(feather)   # for writing out results
+library(readr)     # for csv reading
 library(lubridate) # for date arithmetic in the config file
 
-setwd("shiny-apps/nzfpg-explorer")
+# setwd("shiny-apps/nzfpg-explorer")
 
 # __________________________________________________
 #
 # Read data in from the CSV files
 # __________________________________________________
 
+# Generation file column spec
+gen_cols <- cols(.default     = col_double(),
+                 Site_Code    = col_character(),
+                 POC_Code     = col_character(),
+                 Nwk_Code     = col_character(),
+                 Gen_Code     = col_character(),
+                 Fuel_Code    = col_character(),
+                 Tech_Code    = col_character(),
+                 Trading_date = col_date(format = ""),
+                 TP49         = col_double(),
+                 TP50         = col_double())
+
+# Final prices file column spec
+fp_cols <- cols(Trading_date   = col_date(format = ""),
+                Trading_period = col_integer(),
+                Node           = col_character(),
+                Price          = col_double())
+
 # Nice function to load up multiple files into one data frame
 # From http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
 #      http://www.r-bloggers.com/author/tony-cookson/
-multmerge = function(thepath, filter){
+multmerge = function(thepath, filter, col_spec){
   filenames = list.files(path = thepath, filter, full.names = TRUE)
-  datalist  = lapply(filenames, function(x) {read.csv(file = x, header = TRUE, stringsAsFactors = FALSE)})
+  datalist  = lapply(filenames, function(x) {read_csv(file = x, col_types = col_spec)})
   Reduce(function(x,y) {rbind(x, y)}, datalist)
 }
 
-generation  <- multmerge("data/downloads", "*Generation_MD.csv")
-finalprices <- multmerge("data/downloads", "*Final_prices.csv")
+generation  <- multmerge("data/downloads", "*Generation_MD.csv", gen_cols)
+finalprices <- multmerge("data/downloads", "*Final_prices.csv",  fp_cols)
 
 # __________________________________________________
 #
@@ -35,17 +54,11 @@ union_nodes <- union(fpnodes, gnnodes)
 # Transform into tidy data
 generation <- generation %>%
   melt(id=1:7, na.rm = TRUE) %>%
-  transmute(Trading_date   = as.POSIXct(Trading_date), 
+  transmute(Trading_date   = Trading_date, 
             Trading_period = as.integer(substr(variable, 3, 4) ), 
             Node           = POC_Code, 
             kwh            = value, 
             Fuel_Code      = Fuel_Code)
-
-finalprices <- finalprices %>%
-  transmute(Trading_date   = as.POSIXct(Trading_date), 
-            Trading_period = as.integer(Trading_period),
-            Node           = Node,
-            Price          = Price) 
 
 # Aggregate Generation based on date, period and node only
 generation_aggr <- generation %>%
@@ -64,11 +77,14 @@ write_feather(generation_aggr, "data/generation_aggr.feather")
 
 
 # Create the config file to drive the min/max parameters
-nzfpg_config <- list("min_price"  = min(finalprices$Price),
+max_date   <- max(finalprices$Trading_date)
+min_price  <- min(finalprices$Price)
+
+nzfpg_config <- list("min_price"  = min_price,
                      "max_price"  = max(finalprices$Price),
                      "min_date"   = min(finalprices$Trading_date),
-                     "max_date"   = max(finalprices$Trading_date),
-                     "from_date"  = max_date - months(1),
+                     "max_date"   = max_date,
+                     "from_date"  = max_date - 14,
                      "to_date"    = max_date,
                      "from_price" = min_price,
                      "to_price"   = quantile(finalprices$Price)[[3]])
