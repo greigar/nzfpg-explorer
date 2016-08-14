1bc8afc6041aed66f47fd90f4de25dc4fff120dd
greigar <greigar@gmail.com>
2016-08-14 20:21:35 +1200

    Removed for loop

--- scripts/10-final-gen-get.R
+++ scripts/10-final-gen-get.R
@@ -1,6 +1,8 @@
 library(lubridate) # for date arithmetic
 library(magrittr)  # for piping %>%
-library(readr)     # for reading csv
+
+month_range <- 0:11                  # range of months to download
+year_ago    <- Sys.Date() - years(1) # start from date
 
 old_dir  <- getwd()
 data_dir <- "~/data/electricity/downloads"
@@ -8,26 +10,18 @@ dir.create(data_dir, recursive=TRUE, showWarnings=FALSE)
 setwd(data_dir)
 
 base_url <- "http://www.emi.ea.govt.nz/Datasets/Wholesale/"
-url_fp   <- paste0(base_url, "Final_pricing/Final_prices/")
-url_gen  <- paste0(base_url, "Generation/Generation_MD/")
+urls <- c(paste0(base_url, "Final_pricing/Final_prices/<month>_Final_prices.csv"),
+          paste0(base_url, "Generation/Generation_MD/<month>_Generation_MD.csv"))
 
-year_ago <- Sys.Date() - years(2) + months(5)
+download <- function(download_period, url_type) {
+  month_year   <- format(year_ago + months(download_period), "%Y%m")
+  csv_data_url <- sub("<month>", month_year, url_type)
+  file_name    <- basename(csv_data_url)
 
-read_write <- function(url, file_name) {
-  csv_data_url <- paste0(url,  file_name)
-  print(csv_data_url)
-  read.csv(csv_data_url, stringsAsFactors = FALSE) %>%
-    write.csv(file_name, row.names = FALSE, quote = FALSE)
+  download.file(csv_data_url, file_name)
 }
 
-for(ix in 0:11) {
-  month_year <- format(year_ago + months(ix), "%Y%m")
-
-  file_name_fp  <- paste0(month_year, "_Final_prices.csv") 
-  file_name_gen <- paste0(month_year, "_Generation_MD.csv") 
-
-  read_write(url_fp,  file_name_fp)
-  read_write(url_gen, file_name_gen)
-}
+lapply(urls, function(url)
+               lapply(month_range, function(download_period) download(download_period, url)))
 
 setwd(old_dir)
