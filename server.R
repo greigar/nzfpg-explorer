4a5239862d9ea16afefbbee4e395a7114f7c5f21
greigar <greigar@gmail.com>
2016-08-30 17:15:34 +1200

    Bug fixes

--- server.R
+++ server.R
@@ -8,23 +8,28 @@ shinyServer(
       args                    <- input$node
       args$trading_period_min <- input$range[1]
       args$trading_period_max <- input$range[2]
-      args$price_min          <- input$price[1]
-      args$price_max          <- input$price[2]
       args$trading_date_min   <- input$trading_date[1]
       args$trading_date_max   <- input$trading_date[2]
       args
     }
 
-    output$priceplot <- renderPlot({
+    create_params_prices <- function(input) {
       args <- create_params(input)
+      args$price_min <- input$price[1]
+      args$price_max <- input$price[2]
+      args
+    }
+
+    output$priceplot <- renderPlot({
+      args <- create_params_prices(input)
       filter_formula <- do.call(create_filter_formula, args)
-      prices(create_filter_formula)
+      prices(filter_formula)
     })
 
     output$pricehex  <- renderPlot({
-      args <- create_params(input)
+      args <- create_params_prices(input)
       filter_formula <- do.call(create_filter_formula, args)
-      pricehex(create_filter_formula)
+      pricehex(filter_formula)
     })
 
     output$genplot <- renderPlot({
